5292b6d10d75e8f8c9daf77b9670bde951363f95
Yuxian Gu <guyx21@mails.tsinghua.edu.cn>
2025-09-28 15:59:18 +0800

    fix chunk comba

--- fla/ops/comba/chunk.py
+++ fla/ops/comba/chunk.py
@@ -229,7 +229,7 @@ class ChunkCombaFunction(torch.autograd.Function):
             dq = l2norm_bwd(q, q_rstd, dq)
             dk = l2norm_bwd(k, k_rstd, dk)
             dp = l2norm_bwd(p, p_rstd, dp)
-        return dq.to(q), dk.to(k), dv.to(v), dp.to(p), dg.to(g), db.to(beta), None, dh0, None, None, None
+        return dq.to(q), dk.to(k), dv.to(v), dp.to(p), dg.to(g), db.to(beta), None, dh0, None, None, None, None
 
 
 @torch.compiler.disable
